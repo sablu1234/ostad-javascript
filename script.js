e1070db3768fc8c24ce15c72ae0c5aa18e121d7d
Sablu Hasan <150502592+sablu1234@users.noreply.github.com>
2025-04-26 08:17:49 +0600

    up

---
+++ script.js
@@ -0,0 +1,17 @@
+// variable, array, loop, conditions, functions
+
+// variable -> var, let, const
+
+// var x;
+// x=10;
+// var x= 'apple';
+// console.log(x);
+
+let x ;
+x= 5;
+ x = 'apple';
+console.log(x);
+
+// const x = 15;
+// x = 8;
+// console.log(x);
