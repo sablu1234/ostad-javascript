9d33860dc17221e65e244ea3ca158a3ff8b5630e
Sablu Hasan <150502592+sablu1234@users.noreply.github.com>
2025-04-28 07:10:51 +0600

    js docs last

---
+++ js docs.js
@@ -0,0 +1,239 @@
+// variable, array, loop, conditions, functions
+
+// variable -> var, let, const
+
+
+// Re-declaration is allowed in var,Re-assignment is allowed in var.
+// var x;
+// x=10;
+// var x= 'apple';
+// console.log(x);
+
+
+// let a Re-declaration is not allowed, but re-assignment is allowed.
+// let x ;
+// x= 5;
+//  x = 'apple';
+// console.log(x);
+
+
+// Re-declaration is not allowed in const, Re-assingment is not allowed.
+// const x = 15;
+// x = 8;
+// console.log(x);
+
+
+// letter ,digits, underscore, dollar sign thakte parbe variable a.
+// msut start with letter.
+// can start with underscore and dollar sign.
+// case sensitive => x and X are different variables.
+// cannot use reserved keywords => const let= 5, const function = "addition"
+
+// cases
+// camelcase => firstName, lastName, fullName = JS
+// snakecase => first_name, last_name, full_name = python
+// kebabcase => first-name, last-name, full-name 
+// pascalcase => FirstName, LastName, FullName = C#
+
+// JS DATA TYPES
+// Number-> integer, float, double=> (0,1,2,3,4,5,6,7,8,9),(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9),(1.23456789)
+// String -> "apple", 'banana', `orange`
+// Boolean -> true, false
+// BigInt -> 1234567890123456789012345678901234567890n
+// Undefined -> undefined
+// Null -> Null
+// Symbol -> Symbol('description')
+// Object->array, object, date
+
+// let y = BigInt("3216464646434357664564654");
+// console.log(y);
+
+// Object Data Types
+
+// Object
+// {} => curly brackets
+// [] =>square brackets
+// () => round brackets
+
+// const person = {
+//     name: "john",
+//     age: 30,
+//     city: "New York",
+//     hasChildren: false,
+// };
+// const y =5;
+// console.log(person.name);
+// console.log(person.age);
+// console.log("My name is Prince");
+// console.log(person['name']);
+
+// console.log("My name is "+ person.name + " and i am " + person.age + " years old.");
+// `` (backtick) => template literal
+// console.log(`my name is ${person.name} and i am ${person.age} years old. ${y}`);
+
+
+// Array
+const fruits = ['apple', 'banana', 'orange'];
+
+const person = ['john', 30, 'New York', false];
+
+
+fruits[0] = 'kiwi';
+
+// adding element end of the array
+// fruits.push('mango');
+// console.log(fruits);
+
+// removing element end of the array
+// fruits.pop();
+// console.log(fruits);
+
+// adding element begining of the array
+// fruits.unshift('pineapple');
+// console.log(fruits);
+
+
+// Removing element begining of the array
+// fruits.shift();
+// console.log(fruits);
+
+// Date
+// const date = new Date("2024-05-28");
+// console.log(date);
+
+// Checking variable type
+// const fristName = "Prince";
+// console.log(typeof fristName);
+
+// Operators
+// Arithmetic => +, -, *, /, %, **, ++, --
+// let x = 5;
+// let y = 10;
+
+// let result = x * y; // addition
+// console.log(result);
+
+// Logical operators => &&, ||, !
+// || => OR
+// && => AND
+// ! => NOT
+
+// T | T = T
+// T | F = T
+// F | T = T    
+// F | F = F
+
+// && T| T = T
+// && T| F = F 
+// && F| T = F
+// && F| F = F
+
+// ! T = F
+// ! F = T
+
+// let x = true;
+// let y = !x;
+// console.log(y);
+
+// Comparison Operators => ==, ===, !=, !==, >, <, >=, <=
+
+// let x= 5 === '5';
+// console.log(x);
+
+// Conditions
+// if, else if, else
+// let age = 20;
+// if(age >= 22){
+//     console.log("You are an adult.");
+// }
+// else{
+//     console.log("You are can\'t minor.");
+// }
+
+// let marks = 85;
+// if(marks>= 90){
+//     console.log("A+");
+// }
+// else if(marks>= 80 && marks<90){
+//     console.log("A");
+// }
+// else if(marks>= 70 && marks<80){
+//     console.log("B+");
+// }
+// else if(marks>= 60 && marks<70){
+//     console.log("B");
+// }
+// else if(marks>= 50 && marks<60){
+//     console.log("C+");
+// }
+// else if(marks>= 40 && marks<50){
+//     console.log("C");
+// }
+// else{
+//     console.log("F");
+// }   
+
+
+// ternary operator
+// let age = 2;
+// age>=18 ? console.log('you are voter') : console.log('your are not voter');
+
+// Nulish coalescing operator
+// const userInput = undefined;
+// const userName = userInput ?? "Guest";
+// console.log(userName);
+
+// let marks = 85;
+
+// switch(marks){
+//     case 90:
+//         console.log("A+");
+//         break;
+//     case 80:
+//         console.log("A");
+//         break;
+//     case 70:
+//         console.log("B+");
+//         break;
+//     case 60:
+//         console.log("B");
+//         break;
+//     case 50:
+//         console.log("C+");
+//         break;
+//     case 40:
+//         console.log("C");
+//         break;
+//     default:
+//         console.log("F");
+// }
+
+
+// Functions
+let x = 5;
+let y = 10;
+let result = x+y;
+console.log(result);
+
+let a = 5;
+let b = 10;
+let sum = a+b;
+console.log(result);
+
+// Function Declaration
+// function add(x,y){
+//     let sum = x+y;
+//     return sum;
+// }
+// let res1= add(5,10);
+// let res2= add(5,15);
+// console.log(res1);
+// console.log(res2);
+
+
+function add(x,y){
+    let sum = x+y;
+   console.log(sum);
+}
+
+add(5,10);
--- script.js
+++ script.js
@@ -1,239 +1,89 @@
-// variable, array, loop, conditions, functions
+// Loops-> For, While, Do While, For In, For Of
 
-// variable -> var, let, const
+// console.log(1) ;
+// console.log(2) ;
+// console.log(3) ;
 
+// let a = 5;
+// a +=2;
+// console.log(a);
 
-// Re-declaration is allowed in var,Re-assignment is allowed in var.
-// var x;
-// x=10;
-// var x= 'apple';
-// console.log(x);
+// for(let i=1; i<3 ; i++){
+//     console.log(i);
+// }
 
-
-// let a Re-declaration is not allowed, but re-assignment is allowed.
-// let x ;
-// x= 5;
-//  x = 'apple';
-// console.log(x);
-
-
-// Re-declaration is not allowed in const, Re-assingment is not allowed.
-// const x = 15;
-// x = 8;
-// console.log(x);
+// let i=1;
+// while(i<3){
+//     console.log(i);
+//     i++;
+// }
 
 
-// letter ,digits, underscore, dollar sign thakte parbe variable a.
-// msut start with letter.
-// can start with underscore and dollar sign.
-// case sensitive => x and X are different variables.
-// cannot use reserved keywords => const let= 5, const function = "addition"
+// let i =1;
+// do{
+//     i++;
+//     console.log(i);
+// }while(i<100);
 
-// cases
-// camelcase => firstName, lastName, fullName = JS
-// snakecase => first_name, last_name, full_name = python
-// kebabcase => first-name, last-name, full-name 
-// pascalcase => FirstName, LastName, FullName = C#
-
-// JS DATA TYPES
-// Number-> integer, float, double=> (0,1,2,3,4,5,6,7,8,9),(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9),(1.23456789)
-// String -> "apple", 'banana', `orange`
-// Boolean -> true, false
-// BigInt -> 1234567890123456789012345678901234567890n
-// Undefined -> undefined
-// Null -> Null
-// Symbol -> Symbol('description')
-// Object->array, object, date
-
-// let y = BigInt("3216464646434357664564654");
-// console.log(y);
-
-// Object Data Types
-
-// Object
-// {} => curly brackets
-// [] =>square brackets
-// () => round brackets
 
 // const person = {
-//     name: "john",
-//     age: 30,
-//     city: "New York",
-//     hasChildren: false,
+    
+//     name:'john',
+//     age:30,
+//     email: 'abc@example.com',
 // };
-// const y =5;
-// console.log(person.name);
-// console.log(person.age);
-// console.log("My name is Prince");
-// console.log(person['name']);
 
-// console.log("My name is "+ person.name + " and i am " + person.age + " years old.");
-// `` (backtick) => template literal
-// console.log(`my name is ${person.name} and i am ${person.age} years old. ${y}`);
+// for(const key in person){
+//     console.log(person[key]);
+// }
 
-
-// Array
 const fruits = ['apple', 'banana', 'orange'];
-
-const person = ['john', 30, 'New York', false];
-
-
-fruits[0] = 'kiwi';
-
-// adding element end of the array
-// fruits.push('mango');
-// console.log(fruits);
-
-// removing element end of the array
-// fruits.pop();
-// console.log(fruits);
-
-// adding element begining of the array
-// fruits.unshift('pineapple');
-// console.log(fruits);
-
-
-// Removing element begining of the array
-// fruits.shift();
-// console.log(fruits);
-
-// Date
-// const date = new Date("2024-05-28");
-// console.log(date);
-
-// Checking variable type
-// const fristName = "Prince";
-// console.log(typeof fristName);
-
-// Operators
-// Arithmetic => +, -, *, /, %, **, ++, --
-// let x = 5;
-// let y = 10;
-
-// let result = x * y; // addition
-// console.log(result);
-
-// Logical operators => &&, ||, !
-// || => OR
-// && => AND
-// ! => NOT
-
-// T | T = T
-// T | F = T
-// F | T = T    
-// F | F = F
-
-// && T| T = T
-// && T| F = F 
-// && F| T = F
-// && F| F = F
-
-// ! T = F
-// ! F = T
-
-// let x = true;
-// let y = !x;
-// console.log(y);
-
-// Comparison Operators => ==, ===, !=, !==, >, <, >=, <=
-
-// let x= 5 === '5';
-// console.log(x);
-
-// Conditions
-// if, else if, else
-// let age = 20;
-// if(age >= 22){
-//     console.log("You are an adult.");
-// }
-// else{
-//     console.log("You are can\'t minor.");
+// for(const fruit of fruits){
+//     console.log(fruit);
 // }
 
-// let marks = 85;
-// if(marks>= 90){
-//     console.log("A+");
-// }
-// else if(marks>= 80 && marks<90){
-//     console.log("A");
-// }
-// else if(marks>= 70 && marks<80){
-//     console.log("B+");
-// }
-// else if(marks>= 60 && marks<70){
-//     console.log("B");
-// }
-// else if(marks>= 50 && marks<60){
-//     console.log("C+");
-// }
-// else if(marks>= 40 && marks<50){
-//     console.log("C");
-// }
-// else{
-//     console.log("F");
-// }   
-
-
-// ternary operator
-// let age = 2;
-// age>=18 ? console.log('you are voter') : console.log('your are not voter');
-
-// Nulish coalescing operator
-// const userInput = undefined;
-// const userName = userInput ?? "Guest";
-// console.log(userName);
-
-// let marks = 85;
-
-// switch(marks){
-//     case 90:
-//         console.log("A+");
-//         break;
-//     case 80:
-//         console.log("A");
-//         break;
-//     case 70:
-//         console.log("B+");
-//         break;
-//     case 60:
-//         console.log("B");
-//         break;
-//     case 50:
-//         console.log("C+");
-//         break;
-//     case 40:
-//         console.log("C");
-//         break;
-//     default:
-//         console.log("F");
+// for(let i=0; i<fruits.length; i++){
+//     console.log(fruits[i]);
 // }
 
 
-// Functions
-let x = 5;
-let y = 10;
-let result = x+y;
-console.log(result);
+// const numbers = [1, 2, 3, 4, 5];
+// numbers.forEach(n=>console.log(n*2));
 
-let a = 5;
-let b = 10;
-let sum = a+b;
-console.log(result);
+// Array destructuring
+const numbers = [1, 2, 3];
+// const n1 =numbers[0];
+// const n2 =numbers[1];
 
-// Function Declaration
+// const [n1,n2] = numbers;
+// console.log(n1,n2);
+
+// const [n1, ,n3] =numbers;
+// console.log(n1,n3);
+
+// Object destructuring
+// const person ={
+//     name:'john',
+//     age:30,
+// }
+
+// const name = person.name;
+// const age = person.age;
+
+// const {name, age} = person;
+// console.log(name, age);
+
 // function add(x,y){
-//     let sum = x+y;
-//     return sum;
+//     console.log(x+y);
 // }
-// let res1= add(5,10);
-// let res2= add(5,15);
-// console.log(res1);
-// console.log(res2);
+// add(3,5);
 
+// const add = (x,y) =>{
+//     console.log(x+y);
+// }
+// add(5,8);
+// OR
+// const add = (x,y) =>console.log(x+y);
 
-function add(x,y){
-    let sum = x+y;
-   console.log(sum);
-}
-
-add(5,10);
+const y = x =>  console.log(x*8);
+y(5);
